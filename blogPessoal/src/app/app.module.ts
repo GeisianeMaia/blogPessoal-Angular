351198fd257e43de4cb1c9ed6d16a5f3b09efeca
GeisianeMaia <geisiane.maia@gmail.com>
2020-04-08 11:44:50 -0300

    Criação dos componentes: home, feed, navbar e footer

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -3,18 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { FeedComponent } from './feed/feed.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    FeedComponent,
-    NavbarComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
